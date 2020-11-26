ad5b1b7e5d7cd88949e960534f4a60ba719a5f27
petercolella <peter.joseph.colella@gmail.com>
2020-11-25 20:38:15 -0500

    adds createStateJSONFiles function to data.js

--- data.js
+++ data.js
@@ -2,6 +2,20 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
+const createStateJSONFiles = (data) => {
+  data.forEach((state) => {
+    const stateData = JSON.stringify(state);
+    fs.writeFile(
+      path.join(__dirname, `data/${state.state_slug}.json`),
+      stateData,
+      (err) => {
+        if (err) throw err;
+        console.log(`Data written to data/${state.state_slug}.json`);
+      }
+    );
+  });
+};
+
 const createValueAndNameArray = (data) => {
   const arr = [];
 
@@ -16,7 +30,7 @@ const createValueAndNameArray = (data) => {
 
   fs.writeFile(
     path.join(__dirname, "data/state-names.json"),
-    JSON.stringify(arr),
+    JSON.stringify(arr, null, 2),
     (err) => {
       if (err) throw err;
       console.log(`Array of length: ${arr.length} added to state-names.json`);
@@ -33,18 +47,7 @@ const getData = () => {
       const data = response.data.data.races;
 
       createValueAndNameArray(data);
-
-      data.forEach((state) => {
-        const stateData = JSON.stringify(state);
-        fs.writeFile(
-          path.join(__dirname, `data/${state.state_slug}.json`),
-          stateData,
-          (err) => {
-            if (err) throw err;
-            console.log(`Data written to data/${state.state_slug}.json`);
-          }
-        );
-      });
+      createStateJSONFiles(data);
     });
 };
 
